86ace7cb899a59b2c2e9c7d9fb1b24987bd73e97
Samsung <bitup.games@gmail.com>
2022-11-15 14:17:05 +0100

    fix date format

--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -2,11 +2,11 @@ export default function parseDate(datetime: string): string {
   const date = new Date(datetime);
   const today = new Date();
   const daysAgo = Math.abs(today.getDate() - date.getDate());
-
-  if (daysAgo === 0) {
+  const monthsAgo = Math.abs(today.getMonth() - date.getMonth());
+  if (daysAgo === 0  && monthsAgo === 0) {
     const hoursAgo = today.getHours() - date.getHours();
     return `${hoursAgo} hours ago`;
-  } else if (daysAgo === 1) {
+  } else if (daysAgo === 1 && monthsAgo === 0) {
     return `${daysAgo} day ago`;
   } else {
     return date.toLocaleDateString();
