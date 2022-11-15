3f6bc8d99f9add66a777f4f4b252c2bd7fedbff0
Samsung <bitup.games@gmail.com>
2022-11-15 20:06:39 +0100

    fix lint errors

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,9 +1,9 @@
 export default function formatValue(value: string) {
   const split = value.split('.');
   if (split[1] && split[0].length < 5) {
-    if (split[1].startsWith("0")) {
+    if (split[1].startsWith('0')) {
       return `${split[0]}.${split[1].substring(0, 3)}`;
-    } else if (split[1].startsWith("00")) {
+    } else if (split[1].startsWith('00')) {
       return `${split[0]}.${split[1].substring(0, 4)}`;
     } else {
       return `${split[0]}.${split[1].substring(0, 2)}`;
