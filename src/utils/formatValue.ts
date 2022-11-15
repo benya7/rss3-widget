967f517b03e1931552e18828f202ceef8143c04f
Samsung <bitup.games@gmail.com>
2022-11-15 14:14:56 +0100

    update value format

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,20 +1,20 @@
 export default function formatValue(value: string) {
-  if (value.includes('.')) {
-    const split = value.split('.');
-    if (split[1].startsWith('0')) {
+  const split = value.split('.');
+  if (split[1] && split[0].length < 5) {
+    if (split[1].startsWith("0")) {
       return `${split[0]}.${split[1].substring(0, 3)}`;
-    } else if (split[1].startsWith('00')) {
+    } else if (split[1].startsWith("00")) {
       return `${split[0]}.${split[1].substring(0, 4)}`;
     } else {
       return `${split[0]}.${split[1].substring(0, 2)}`;
     }
   } else {
-    if (value.length > 4 && value.length <= 6) {
-      return `${value.substring(0, value.length - 3)}K`;
-    } else if (value.length > 6) {
-      return `${value.substring(0, value.length - 6)}M`;
+    if (split[0].length > 4 && split[0].length <= 6) {
+      return `${split[0].substring(0, split[0].length - 3)}K`;
+    } else if (split[0].length > 6) {
+      return `${split[0].substring(0, split[0].length - 6)}M`;
     } else {
-      return value;
+      return split[0];
     }
   }
 }
