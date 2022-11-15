5bf587fef3e1169c91c9861376db9d631b80f8b0
Samsung <bitup.games@gmail.com>
2022-11-15 20:08:58 +0100

    fix erros in parse date

--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -4,8 +4,13 @@ export default function parseDate(datetime: string): string {
   const daysAgo = Math.abs(today.getDate() - date.getDate());
   const monthsAgo = Math.abs(today.getMonth() - date.getMonth());
   if (daysAgo === 0  && monthsAgo === 0) {
-    const hoursAgo = today.getHours() - date.getHours();
-    return `${hoursAgo} hours ago`;
+    const hoursAgo = Math.abs(today.getHours() - date.getHours());
+    if (hoursAgo > 0) {
+      return `${hoursAgo} hours ago`;
+    } else {
+      const minutesAgo = Math.abs(today.getMinutes() - date.getMinutes());
+      return `${minutesAgo} minutes ago`;
+    }
   } else if (daysAgo === 1 && monthsAgo === 0) {
     return `${daysAgo} day ago`;
   } else {
